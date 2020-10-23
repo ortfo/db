6e7406ad9aa52b7843706b424673c39f133351e3
Ewen Le Bihan <hey@ewen.works>
2020-10-23 22:56:07 +0200

    🚧 Start working on color extraction

--- building.go
+++ building.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/EdlinOrg/prominentcolor"
+	"image"
+)
+
+// ExtractColors returns (primary, secondary, tertiary) hexstrings of colors (without the # in front--just the hexstring)
+func ExtractColors(img image.Image) (string, string, string, error) {
+	centroids, err := prominentcolor.Kmeans(img)
+	if err != nil {
+		return "", "", "", err
+	}
+	colors := make([]string, 0, 3)
+	for _, centroid := range centroids {
+		colors = append(colors, centroid.AsString())
+	}
+	return colors[0], colors[1], colors[2], nil
+}
+
+//TODO: convert GIFs from `online: True` sources (YouTube, Dailymotion, Vimeo, you name it.). Might want to look at <https://github.com/hunterlong/gifs>
